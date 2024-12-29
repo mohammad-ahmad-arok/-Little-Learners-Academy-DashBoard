f90bc3c2114351ba299d3c860cb7ed93dd2d7aec
AbdAlrahman13 <blalabo89@gmail.com>
2024-12-29 07:53:17 +0300

    api url

--- src/services/axios-global.ts
+++ src/services/axios-global.ts
@@ -1,2 +1,2 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://little-learners-academy-back-end-z155.onrender.com";
+axios.defaults.baseURL = "https://little-learners-academy-back-end-bfbn.onrender.com";
